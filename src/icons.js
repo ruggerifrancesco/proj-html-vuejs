978c90db4389237058d1ccd3a8914b233ab98657
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-26 15:28:52 +0200

    created new importing file for fonta awesome icons

---
+++ src/icons.js
@@ -0,0 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+// Single Icons Handling
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+// Add icons to the library
+library.add(faUserSecret)
+
+export { FontAwesomeIcon }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { FontAwesomeIcon } from './icons.js'
 
-createApp(App).mount('#app')
+createApp(App)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
