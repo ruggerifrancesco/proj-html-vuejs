d998261e35946c3048bd2f85edd3f9e21e13a4bd
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-26 16:25:35 +0200

    setup header background measures and image

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,7 +12,10 @@ export default {
 
 <style lang="scss" scoped>
     header {
-        min-height: 100vh;
-        background-image: url(../assets/);
+        min-height: calc(100vh);
+        background-image: url(../assets/hero/avadabarbers-homepage-hero-bg.jpg);
+        background-position: left top;
+        background-repeat: no-repeat;
+        background-size: cover;
     }
 </style>
