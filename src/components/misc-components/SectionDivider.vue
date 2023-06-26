130e78c060590a7eda82ac46dcdc8ae086bb68a1
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-26 17:02:16 +0200

    Added new component project for section dividing

---
+++ src/components/misc-components/SectionDivider.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="section-divider">
+        <img src="../../assets/triangle.svg" alt="Section Divider">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SectionDivider',
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/_mixins.scss' as *;
+@use '../../styles/partials/_variables.scss' as *;
+
+    .section-divider {
+        height: 100px;
+
+        img {
+            @include imgElement (100%, null, null);
+        }
+    }
+    
+</style>
