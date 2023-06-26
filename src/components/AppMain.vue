3ff701e25a44a6a266922296175e4ca59ba60818
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-26 17:02:31 +0200

    tested new component into section, defined background and siziing

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,34 +1,56 @@
 <template>
     <main>
         <!-- Awards Section -->
-        <section></section>
+        <section id="awards" class="awards-container">
+            <SectionDivider class="sectionDivider" />
+        </section>
 
         <!-- Services Section -->
-        <section></section>
+        <section id="services"></section>
 
         <!-- Products Section -->
-        <section>
+        <section id="products">
             <!-- Component And for in JS -->
         </section>
 
         <!-- Reviews Section -->
-        <section>
+        <section id="reviews">
             <!-- Component And for in JS -->
         </section>
 
         <!-- Blog Section -->
-        <section>
+        <section id="blog">
             <!-- Component And for in JS -->
         </section>
     </main>
 </template>
 
 <script>
+import SectionDivider from '../components/misc-components/SectionDivider.vue'
+
 export default {
     name: 'AppMain',
+    components: {
+        SectionDivider, 
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    
+@use '../styles/partials/_mixins.scss' as *;
+@use '../styles/partials/_variables.scss' as *;
+
+    .awards-container {
+        height: 450px;
+        @include background ('../assets/other/avadabarbers-about-introbackground.jpg', cover, center top, transparent);
+        position: relative;
+
+        .sectionDivider {
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    }
+
 </style>
