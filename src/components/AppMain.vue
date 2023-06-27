8f41dded4731548a9920175610f24d2bdfe6788b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-27 18:04:35 +0200

    added services and stgyled spacing between columns

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,6 +14,46 @@
 
         <!-- Services Section -->
         <section id="services" class="services-container">
+            <div class="section-wrapper">
+                <span class="section-subtitle">Mens Grooming</span>
+                <h3>Services</h3>
+
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            <div class="service-icon">
+                                <img src="../assets/icons/avadabarbers-trimcut-icon-before.png" alt="">
+                            </div>
+                            <h4>Trim & Cut</h4>
+                            <p>
+                                Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.
+                            </p>
+                        </div>
+                        <div class="col">
+                            <div class="service-icon">
+                                <img src="../assets/icons/avadabarbers-washndry-icon.png" alt="">
+                            </div>
+                            <h4>Wash & Dry</h4>
+                            <p>
+                                Take a seat in our fine leather chairs, lean back and let us lather you a fresh head in a luxurious fashion.
+                            </p>
+                        </div>
+                        <div class="col">
+                            <div class="service-icon">
+                                <img src="../assets/icons/avadabarbers-beardtrim-icon.png" alt="">
+                            </div>
+                            <h4>Beard Tidy</h4>
+                            <p>
+                                Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <a href="#" class="btn-custom">
+                    Read about our services
+                </a>
+            </div>
             <SectionDivider class="sectionDivider" />
         </section>
 
@@ -76,9 +116,21 @@ export default {
     }
 
     .services-container {
-        height: 900px;
         position: relative;
         background-color: white;
+        padding: $wrapper-padding-xs;
+        text-align: center;
+
+        .section-wrapper {
+            .row {
+                @include flex (column, flex-start, center);
+
+                .service-icon img {
+                    @include imgElement (auto, contain, center top);
+                    aspect-ratio: 5/1;
+                }
+            }
+        }
 
         .sectionDivider {
             filter: invert(0.95);
@@ -98,26 +150,6 @@ export default {
         }
     }
 
-    @media screen and (min-width: 1024px) {
-
-        h3 {
-            font-size: 5rem;
-        }
-        .product-special-container {
-            display: block !important;
-            height: 450px;
-            max-width: calc( 1140px + 0px );
-            background-color: black;
-            border-top: 4px solid red;
-            margin: -50px auto 0;
-        }
-        .awards-container {
-            .section-wrapper {
-                padding: 28px 0 78px 0;
-            }
-        }
-
-    }
     .reviews-container {
         height: 750px;
         background-color: white;
@@ -140,6 +172,61 @@ export default {
         bottom: 0;
         left: 0;
         right: 0;
+    }
+
+    @media (min-width: 576px) {
+        .services-container {
+            padding: $wrapper-padding-sm;
+        }
+    }
+
+    @media (min-width: 768px) {
+        .services-container {
+            padding: $wrapper-padding-md;
+
+            .section-wrapper {
+                .row {
+                    .service-icon img {
+                        aspect-ratio: 4.5/1;
+                    }
+                }
+            }
+        }
+    }
+
+    @media screen and (min-width: 1024px) {
+
+        h3 {
+            font-size: 5rem;
+        }
+        .product-special-container {
+            display: block !important;
+            height: 450px;
+            max-width: calc( 1140px + 0px );
+            background-color: black;
+            border-top: 4px solid red;
+            margin: -50px auto 0;
+        }
+        .awards-container {
+            .section-wrapper {
+                padding: 28px 0 78px 0;
+            }
+        }
+
+        .services-container {
+            .section-wrapper {
+                max-width: $wrapper-maxw;
+                margin: 0 auto;
+                padding: 28px 0 15rem 0;
+                .row {
+                    @include flex (row, flex-start, flex-start);
+
+                    .service-icon img {
+                        aspect-ratio: 3.2/1;
+                    }
+                }
+            }
+        }
     }
 
 </style>
