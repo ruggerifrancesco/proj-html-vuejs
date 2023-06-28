a5e2b47eb5c4e5e28d24e4338a4e7c66e99e4fa7
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-28 12:05:48 +0200

    fixed products list container

--- src/components/main-related/ProductsSection.vue
+++ src/components/main-related/ProductsSection.vue
@@ -2,8 +2,18 @@
     <section id="products" class="products-container">
 
         <!-- Component And for in JS -->
-        <div class="products-list-container"></div>
-        <div class="product-special-container"></div>
+        <div class="products-list-mobile"></div>
+        <div class="products-list-desktop"></div>
+        <div class="product-special-container">
+            <div class="image-wrapper">
+                <img src="../../assets/products/shaving_butter.png" alt="">
+            </div>
+
+            <div class="content-product">
+                <span class="section-subtitle">Mens Grooming</span>
+                <h3>Services</h3>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -19,13 +29,14 @@ export default {
 
     .products-container {
         background-color: white;
-        .products-list-container {
-            height: 790px;
-            @include background ('../../assets/other/avadabarbers-reviewsbackground.jpg', cover, center top, transparent)
 
+        .products-list-mobile,
+        .products-list-desktop {
+            height: 790px;
+            @include background ('../../assets/other/avadabarbers-reviewsbackground.jpg', cover, center top, transparent);
         }
 
-        .product-special-container {
+        .products-list-desktop {
             display: none;
         }
     }
@@ -36,17 +47,33 @@ export default {
     }
 
     @media screen and (min-width: 1024px) {
-        .product-special-container {
-            display: block !important;
-            height: 450px;
-            max-width: calc( 1140px + 0px );
-            background-color: black;
-            border-top: 4px solid red;
-            margin: -50px auto 0;
-        }
-        .awards-container {
-            .section-wrapper {
-                padding: 28px 0 78px 0;
+        .products-container {
+
+            .products-list-desktop {
+                display: block;
+            }
+            .products-list-mobile {
+                display: none;
+            }
+
+            .product-special-container {
+                display: block !important;
+                height: 450px;
+                max-width: calc( 1140px + 0px );
+                background-color: black;
+                border-top: 4px solid #be9359;
+                margin: -50px auto 0;
+                @include flex (row, flex-start, null);
+
+                .image-wrapper {
+                    width: 40%;
+                    height: 100%;
+                    @include background ('../../assets/other/avadabarbers-cta-background.jpg', cover, center, transparent);
+
+                    img {
+                        @include imgElement ( 100%, cover, top center);
+                    }
+                }
             }
         }
     }
