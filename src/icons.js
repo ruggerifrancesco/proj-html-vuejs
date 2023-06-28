496edba4b8bddfcf64e182b8fe98b33b8c98c2e2
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-28 15:38:29 +0200

    imported new arrow icons

--- src/icons.js
+++ src/icons.js
@@ -4,9 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 // Single Icons Handling
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 
 // Add icons to the library
-library.add(faBars);
+library.add(faBars, faChevronLeft, faChevronRight);
 
 export { FontAwesomeIcon }
