fb2cb5ac4fcc4c97b7a4428ba9862e45352dac60
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-28 10:04:49 +0200

    created n ew component to have more division for services

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -12,50 +12,7 @@
             <SectionDivider class="sectionDivider" />
         </section>
 
-        <!-- Services Section -->
-        <section id="services" class="services-container">
-            <div class="section-wrapper">
-                <span class="section-subtitle">Mens Grooming</span>
-                <h3>Services</h3>
-
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col">
-                            <div class="service-icon">
-                                <img src="../assets/icons/avadabarbers-trimcut-icon-before.png" alt="">
-                            </div>
-                            <h4>Trim & Cut</h4>
-                            <p>
-                                Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.
-                            </p>
-                        </div>
-                        <div class="col">
-                            <div class="service-icon">
-                                <img src="../assets/icons/avadabarbers-washndry-icon.png" alt="">
-                            </div>
-                            <h4>Wash & Dry</h4>
-                            <p>
-                                Take a seat in our fine leather chairs, lean back and let us lather you a fresh head in a luxurious fashion.
-                            </p>
-                        </div>
-                        <div class="col">
-                            <div class="service-icon">
-                                <img src="../assets/icons/avadabarbers-beardtrim-icon.png" alt="">
-                            </div>
-                            <h4>Beard Tidy</h4>
-                            <p>
-                                Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <a href="#" class="btn-custom">
-                    Read about our services
-                </a>
-            </div>
-            <SectionDivider class="sectionDivider" />
-        </section>
+        <ServicesSection />
 
         <!-- Products Section -->
         <section id="products" class="products-container">
@@ -80,11 +37,13 @@
 
 <script>
 import SectionDivider from '../components/misc-components/SectionDivider.vue'
+import ServicesSection from './main-related/ServicesSection.vue';
 
 export default {
     name: 'AppMain',
     components: {
-        SectionDivider, 
+        SectionDivider,
+        ServicesSection, 
     }
 }
 </script>
@@ -97,12 +56,6 @@ export default {
         margin-bottom: 620px;
     }
 
-    h3 {
-        color: #b0b0b0;
-        font-size: 3rem;
-        margin-bottom: 30px;
-    }
-
     .awards-container {
         @include background ('../assets/other/avadabarbers-about-introbackground.jpg', cover, center top, transparent);
         position: relative;
@@ -112,28 +65,6 @@ export default {
         .section-wrapper {
             width: 100%;
             padding: 0;
-        }
-    }
-
-    .services-container {
-        position: relative;
-        background-color: white;
-        padding: $wrapper-padding-xs;
-        text-align: center;
-
-        .section-wrapper {
-            .row {
-                @include flex (column, flex-start, center);
-
-                .service-icon img {
-                    @include imgElement (auto, contain, center top);
-                    aspect-ratio: 5/1;
-                }
-            }
-        }
-
-        .sectionDivider {
-            filter: invert(0.95);
         }
     }
 
@@ -167,38 +98,13 @@ export default {
         }
     }
 
-    .sectionDivider {
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        right: 0;
+    @media screen and (min-width: 576px) {
     }
 
-    @media (min-width: 576px) {
-        .services-container {
-            padding: $wrapper-padding-sm;
-        }
-    }
-
-    @media (min-width: 768px) {
-        .services-container {
-            padding: $wrapper-padding-md;
-
-            .section-wrapper {
-                .row {
-                    .service-icon img {
-                        aspect-ratio: 4.5/1;
-                    }
-                }
-            }
-        }
+    @media screen and (min-width: 768px) {
     }
 
     @media screen and (min-width: 1024px) {
-
-        h3 {
-            font-size: 5rem;
-        }
         .product-special-container {
             display: block !important;
             height: 450px;
@@ -210,21 +116,6 @@ export default {
         .awards-container {
             .section-wrapper {
                 padding: 28px 0 78px 0;
-            }
-        }
-
-        .services-container {
-            .section-wrapper {
-                max-width: $wrapper-maxw;
-                margin: 0 auto;
-                padding: 28px 0 15rem 0;
-                .row {
-                    @include flex (row, flex-start, flex-start);
-
-                    .service-icon img {
-                        aspect-ratio: 3.2/1;
-                    }
-                }
             }
         }
     }
---
+++ src/components/main-related/ServicesSection.vue
@@ -0,0 +1,116 @@
+<template>
+    <section id="services" class="services-container">
+        <div class="section-wrapper">
+            <span class="section-subtitle">Mens Grooming</span>
+            <h3>Services</h3>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col">
+                        <div class="service-icon">
+                            <img src="../../assets/icons/avadabarbers-trimcut-icon-before.png" alt="">
+                        </div>
+                        <h4>Trim & Cut</h4>
+                        <p>
+                            Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <div class="service-icon">
+                            <img src="../../assets/icons/avadabarbers-washndry-icon.png" alt="">
+                        </div>
+                        <h4>Wash & Dry</h4>
+                        <p>
+                            Take a seat in our fine leather chairs, lean back and let us lather you a fresh head in a luxurious fashion.
+                        </p>
+                    </div>
+                    <div class="col">
+                        <div class="service-icon">
+                            <img src="../../assets/icons/avadabarbers-beardtrim-icon.png" alt="">
+                        </div>
+                        <h4>Beard Tidy</h4>
+                        <p>
+                            Tame the tangles and untidy facial hairs like a gentleman with our Beard Tidy services from Avada Barbers.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <a href="#" class="btn-custom">
+                Read about our services
+            </a>
+        </div>
+        <SectionDivider class="sectionDivider" />
+    </section>
+
+</template>
+
+<script>
+import SectionDivider from '../misc-components/SectionDivider.vue'
+
+export default {
+    name: 'ServicesSection',
+    components: {
+        SectionDivider, 
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/_mixins.scss' as *;
+@use '../../styles/partials/_variables.scss' as *;
+
+    .services-container {
+        position: relative;
+        background-color: white;
+        padding: $wrapper-padding-xs;
+        text-align: center;
+        .section-wrapper {
+            .row {
+                @include flex (column, flex-start, center);
+                .service-icon img {
+                    @include imgElement (auto, contain, center top);
+                    aspect-ratio: 5/1;
+                }
+            }
+        }
+        .sectionDivider {
+            filter: invert(0.95);
+        }
+    }
+
+    @media screen and (min-width: 576px) {
+    .services-container {
+            padding: $wrapper-padding-sm;
+            }
+    }
+
+    @media screen and (min-width: 768px) {
+        .services-container {
+            padding: $wrapper-padding-md;
+            .section-wrapper {
+                .row {
+                    .service-icon img {
+                        aspect-ratio: 4.5/1;
+                    }
+                }
+            }
+        }
+    }
+    
+    @media screen and (min-width: 1024px) {
+        .services-container {
+            .section-wrapper {
+                max-width: $wrapper-maxw;
+                margin: 0 auto;
+                padding: 28px 0 15rem 0;
+                .row {
+                    @include flex (row, flex-start, flex-start);
+                    .service-icon img {
+                        aspect-ratio: 3.2/1;
+                    }
+                }
+            }
+        }
+    }
+</style>
