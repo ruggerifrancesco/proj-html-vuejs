a0c87cb816c17e5ea23c7a606b84d79dd8ed0963
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-28 21:12:06 +0200

    defined strcutre of vue component

---
+++ src/components/main-related/ReviewsSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- Reviews Section -->
+    <section id="reviews" class="reviews-container">
+
+        <!-- Component And for in JS -->
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ReviewsSection',
+    props: {
+
+    },
+    methods: {
+        getImagePath: function(img) {
+            return new URL(`../../assets/${img}`, import.meta.url).href;
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/partials/_mixins.scss' as *;
+@use '../../styles/partials/_variables.scss' as *;
+
+    .reviews-container {
+        height: 750px;
+        background-color: white;
+    }
+    
+</style>
