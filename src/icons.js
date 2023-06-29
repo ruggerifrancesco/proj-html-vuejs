e033ac339d56005e20558c0a622606585ee4ef5b
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-29 10:25:47 +0200

    imported icons in icons.js

--- src/icons.js
+++ src/icons.js
@@ -7,9 +7,11 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube, faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 
 // Add icons to the library
-library.add(faBars, faChevronLeft, faChevronRight, faCartShopping);
+library.add(faBars, faChevronLeft, faChevronRight, faCartShopping, faTwitter, faYoutube, faFacebookF, faInstagram);
 
 export { FontAwesomeIcon }
