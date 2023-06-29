db9e45a89a7c00b4f1c21ef5cd79a78614b0bde4
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-29 10:16:36 +0200

    defined new structure and responsivness of footers

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,16 +1,34 @@
 <template>
     <footer>
         <section class="footer-primary">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col">
-                        Test
-                    </div>
-                    <div class="col">
-                        Test
-                    </div>
-                    <div class="col">
-                        Test
+            <div class="section-wrapper">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col">
+                            Test
+                        </div>
+                        <div class="col">
+                            <div class="logo-wrapper">
+                                <img src="../assets/logo/avadabarbers-logo-x2.png" alt="Main Logo">
+                            </div>
+                            <ul>
+                                <li>Avada Barbers</li>
+                                <li>123 New York Street</li>
+                                <li>New York City</li>
+                                <li>info@yourwebsite.com</li>
+                                <li>+1 (555) 555-1212</li>
+                            </ul>
+                            <span class="section-subtitle">Follow us</span>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            Test
+                        </div>
                     </div>
                 </div>
             </div>
@@ -35,16 +53,30 @@ export default {
     footer {
        background-color: #0f0f0f; 
        height: $footer-height;
+       position: relative;
     }
 
     .footer-primary,
     .footer-bottom {
-        padding: $wrapper-padding-xs;
+        .section-wrapper {
+            padding: $wrapper-padding-xs;
+        }
     }
 
     .footer-primary {
+        width: 100%;
+        height: 100%;
+        .section-wrapper {
+            @include flex (row, center, center);
+            height: calc(100% - $footer-bottom-height);
+        }
         .container-fluid {
             .row {
+                .col {
+                    padding: 0 3%;
+                    text-align: center;
+                    color: #686868;
+                }
                 .col:first-child,
                 .col:last-child {
                     display: none;
@@ -53,17 +85,30 @@ export default {
         }
     }
 
+    .footer-bottom {
+        background-color: #171717;
+        height: $footer-bottom-height;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
     @media screen and (min-width: 576px) {
         .footer-primary,
         .footer-bottom {
-            padding: $wrapper-padding-sm;
+            .section-wrapper {
+                padding: $wrapper-padding-sm;
+            }
         }
     }
 
     @media screen and (min-width: 768px) {
         .footer-primary,
         .footer-bottom {
-            padding: $wrapper-padding-md;
+            .section-wrapper {
+                padding: $wrapper-padding-md;
+            }
         }
 
         .footer-primary {
@@ -87,11 +132,13 @@ export default {
             right: 0;
             z-index: -1;
         }
-        
+
         .footer-primary,
         .footer-bottom {
-            max-width: $wrapper-maxw;
-            margin: 0 auto;
+                .section-wrapper {
+                    max-width: $wrapper-maxw;
+                    margin: 0 auto;
+                }
             }
     }
 </style>
